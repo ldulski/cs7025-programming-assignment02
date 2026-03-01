f91feae9ee200ad42719a9a29120900e31d57812
Logan Dulski <96384843+ldulski@users.noreply.github.com>
2026-03-01 09:37:33 +0000

    adding light/dark mode switch

--- app/JS/script.js
+++ app/JS/script.js
@@ -1,1 +1,36 @@
 console.log('HELLO')
+
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggle = document.querySelector('#theme-switch');
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+    console.log("Running Disable Function")
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkMode', null);
+}
+
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    console.log(darkMode)
+    if (darkMode != 'enabled') {
+        console.log("Enabling");
+        enableDarkMode();
+    } else {
+        console.log("Disabling");
+        disableDarkMode();
+    }
+});
