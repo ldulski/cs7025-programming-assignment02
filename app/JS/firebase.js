ff636e259489a167261eacc88545918be9a6634d
MinoliMathew <minolimathew@gmail.com>
2026-03-24 17:17:53 +0000

    Database signup/signin functionality and quiz functionality

---
+++ app/JS/firebase.js
@@ -0,0 +1,28 @@
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+//import { getFirestore } from "firebase/firestore";
+//import { getAuth } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+//import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBNQvib46jRfLKbgOiabi-Z515nOTEhaLI",
+    authDomain: "cs7025-programming-postd.firebaseapp.com",
+    projectId: "cs7025-programming-postd",
+    storageBucket: "cs7025-programming-postd.firebasestorage.app",
+    messagingSenderId: "951492731667",
+    appId: "1:951492731667:web:e59f0d35421846c406b0e6",
+    measurementId: "G-WK1FE767GN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+//Authentication
+export const auth = getAuth(app);
+//database
+export const db = getFirestore(app);
+console.log("firebase initialized",auth);
